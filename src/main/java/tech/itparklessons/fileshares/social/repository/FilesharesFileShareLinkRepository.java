c44127e313b1752c09950f341d3200674c291a67
Eduard <mansurov@technokratos.com>
2021-06-24 21:38:35 +0300

    fixed

--- src/main/java/tech/itparklessons/fileshares/social/repository/FilesharesFileShareLinkRepository.java
+++ src/main/java/tech/itparklessons/fileshares/social/repository/FilesharesFileShareLinkRepository.java
@@ -1,6 +1,7 @@
 package tech.itparklessons.fileshares.social.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import tech.itparklessons.fileshares.social.model.entity.FilesharesFileShareLink;
 
@@ -10,6 +11,7 @@ import java.util.UUID;
 public interface FilesharesFileShareLinkRepository extends JpaRepository<FilesharesFileShareLink, Long> {
     FilesharesFileShareLink findByShareLink(String shareLink);
 
+    @Modifying
     @Query(value = "UPDATE fileshares_file_share_link SET deleted = true WHERE fileshares_file_id IN " +
             "(SELECT fileshares_social_file.id FROM fileshares_social_file WHERE files_service_fileuuid IN (:fileUUID))", nativeQuery = true)
     void markAsDeleted(List<UUID> fileUUID);
